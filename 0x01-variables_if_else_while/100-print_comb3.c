a4c6798bccf5372297b7ade93304d42020df2738
Peace <105157187+Peacexxela@users.noreply.github.com>
2022-06-06 19:11:20 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - prints the English alphabet in lowercase
- * and uppercase
+ * main - Prints numbers between 00 to 89.
  *
- * Return: 0 if execution was successful
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
+	int i, e;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		putchar(letter);
-	}
-	for (letter = 'A'; letter <= 'Z'; letter++)
-	{
-		putchar(letter);
-	}
+	i = 48;
+	e = 48;
 
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
 	putchar('\n');
 	return (0);
 }
