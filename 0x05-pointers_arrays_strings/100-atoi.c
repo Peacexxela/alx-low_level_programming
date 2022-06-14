d50165b2393ca47d35371f5e41a8158f8853727e
Peacexxela <peaceowem233@gmail.com>
2022-06-13 19:48:43 -0700

    atoi advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,48 +1,35 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _atoi - Converts a string to an integer
- * @s: The string to be converted
- * Return: The first integer in the string
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
 int _atoi(char *s)
 {
-	short sign = 1;
-	double result = 0;
-	int len, digits_seen_count, i, pass, positive, old_sign;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	len = i = digits_seen_count = pass = old_sign = 0;
-	while (pass <= 1)
+	while (*(s + count) != '\0')
 	{
-		positive = old_sign != 0 ? old_sign == *(s + len) : *(s + len) == '+';
-		if (*(s + len) == '\0')
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
-			pass++;
-			len = 0;
-			continue;
+			if (size > 0)
+				m *= 10;
+			size++;
 		}
-		else if (*(s + len) >= '0' && *(s + len) <= '9')
-		{
-			if (pass == 0)
-				digits_seen_count++;
-			else
-				result += (*(s + len) - '0') * b10_pow(digits_seen_count - i++ - 1);
-		}
-		else
-		{
-			if (pass == 0 && digits_seen_count > 0)
-			{
-				pass++;
-				len -= digits_seen_count;
-				continue;
-			}
-			if ((*(s + len) == '+' || *(s + len) == '-') && digits_seen_count == 0)
-			{
-				sign = positive ? 1 : 0 - 1;
-				old_sign = positive ? '+' : '-';
-			}
-		}
-		len++;
+		count++;
 	}
-	return (result * sign);
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
